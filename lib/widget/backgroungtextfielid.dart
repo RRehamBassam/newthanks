057d940048ce39f7875feea79c36e6509467f521
Reham <reham2alamssi1998@gmail.com>
2021-12-22 01:00:56 +0200

    new

--- lib/widget/backgroungtextfielid.dart
+++ lib/widget/backgroungtextfielid.dart
@@ -8,7 +8,7 @@ class backgroungtextfielid extends StatelessWidget {
   Widget build(BuildContext context) {
           return new Container(
 
-            height: MediaQuery.of(context).size.height*0.08,
+            height: MediaQuery.of(context).size.height*0.08,//
             width: MediaQuery.of(context).size.width*0.82,
       decoration: BoxDecoration(
      // color: Color(0xfff5f6fb),borderRadius: BorderRadius.circular(8.00),
